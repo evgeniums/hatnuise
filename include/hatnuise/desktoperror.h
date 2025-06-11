64f0c443000b7b1803684657ac6b2dcf5a5b20b1
evgeniums <evgeniums@dracosha.com>
2025-06-11 19:28:10 +0300

    Add desktoperror.h

---
+++ include/hatnuise/desktoperror.h
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2024 - current, Evgeny Sidorov (decfile.com), All rights reserved.
+
+    {{LICENSE}}
+*/
+
+/****************************************************************************/
+/*
+    
+*/
+/** @file hatnuise/desktoperror.h
+  */
+
+/****************************************************************************/
+
+#ifndef HATNUISEDESKTOPERROR_H
+#define HATNUISEDESKTOPERROR_H
+
+#include <hatn/common/error.h>
+
+#include <hatnuise/hatnuise.h>
+
+#include <QString>
+
+HATN_UISE_NAMESPACE_BEGIN
+
+struct desktopErrorT
+{
+    QString operator()(const HATN_COMMON::Error& ec, const QString& message={}) const
+    {
+        auto msg=QString::fromStdString(ec.message());
+
+        if (message.isEmpty())
+        {
+            return msg;
+        }
+
+        msg=message+": "+msg;
+        return msg;
+    }
+};
+constexpr desktopErrorT desktopError{};
+
+HATN_UISE_NAMESPACE_END
+
+#endif // HATNUISEDESKTOPERROR_H
